f24150e3b27c563dc8e250a8ada01c34770cdad2
Aswin C <aachu8966@gmail.com>
2024-11-30 20:41:58 +0530

    Update

--- trie/trei.py
+++ trie/trei.py
@@ -1,70 +1,65 @@
-# class TrieNode:
-#     def __init__(self):
-#         self.children={}
-#         self.is_leaf=False
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.is_leaf=False
         
 
 
-# class Trie:
-#     def __init__(self):
-#         self.root=TrieNode()
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
         
-#     def insert(self,word):
-#         node=self.root
-#         for char in word:
-#             if char not in node.children:
-#                 node.children[char]=TrieNode()
-#             # print(node.children[char])
-#             node=node.children[char]
+    def insert(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char]=TrieNode()
+            # print(node.children[char])
+            node=node.children[char]
             
-#         node.is_leaf=True
+        node.is_leaf=True
     
-#     def search(self,word):
-#         node=self.root
-#         for char in word:
-#             if char not in node.children:
-#                 return False
-#             node=node.children[char]
+    def search(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node=node.children[char]
             
-#         return node.is_leaf    
+        return node.is_leaf    
     
-#     def startwith(self,prefix): 
-#         node=self.root
-#         for char in prefix:
-#             if char not in node.children:
-#                 return False
-#             node=node.children[char]
-#         return True
+    def startwith(self,prefix): 
+        node=self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node=node.children[char]
+        return True
     
-#     def print_st(self,prefix):
-#         node=self.root
-#         for char in prefix:
-#             if char not in node.children:
-#                 return False
-#             node=node.children[char]
-#         self.print_st_hr(node,prefix)
+    def print_st(self,prefix):
+        node=self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node=node.children[char]
+        self.print_st_hr(node,prefix)
         
-#     def print_st_hr(self,node,word):
-#         if node.is_leaf:
-#             print(word)
-#         for char,child in node.children.items():
-#             self.print_st_hr(child,word+char)
+    def print_st_hr(self,node,word):
+        if node.is_leaf:
+            print(word)
+        for char,child in node.children.items():
+            self.print_st_hr(child,word+char)
 
             
     
     
         
-# tr=Trie()
-# tr.insert("helllo")
-# tr.insert("hellli")
-# tr.insert("helllu") 
-# print(tr.root.children)
-# print(tr.search("helllo"))
-# print(tr.search("helllu"))
-# print(tr.startwith("helllu"))
-# tr.print_st("hell")
-
-
-
-
-print("Hi i am sreya")
+tr=Trie()
+tr.insert("helllo")
+tr.insert("hellli")
+tr.insert("helllu") 
+print(tr.root.children)
+print(tr.search("helllo"))
+print(tr.search("helllu"))
+print(tr.startwith("helllu"))
+tr.print_st("hell")
